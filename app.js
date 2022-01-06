d260ba09e028d07dd1ee8cd0ccb4c864f80f17c1
Loktysh Alexandr <vebster24@mail.ru>
2022-01-07 02:38:12 +0300

    modified:   app.js 	modified:   package-lock.json 	modified:   package.json

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
-const express = require('express')
+const express = require('express');
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
+var TelegramBot = require('node-telegram-bot-api');
+var token = '1016574486:AAFP1RxH-o5ex7g6a0MdI572JhjbmVDJiU8';
+var bot = new TelegramBot(token, { polling: true });
+var priceURL = 'https://catalog.onliner.by/ssd/westerndigital/wds200t1x0e';
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+function getPrice () {
+  console.log('Start getting')
+  fetch(priceURL)
+  .then(res => res.text())
+  .then(text => {
+    var $ = cheerio.load(text);
+    title = $(".catalog-masthead__title").text().trim();
+    bot.sendMessage(process.env.tgid, `${price}`);
+  })
+}
+setInterval(() => getPrice(), 60000);
